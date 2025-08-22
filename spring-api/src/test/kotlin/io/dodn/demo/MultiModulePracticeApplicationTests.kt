1ec1d79684578b322109e77846e3252d0b9606df
HyeonSeong <gus8131@naver.com>
2025-08-22 18:09:10 +0900

    first commit

---
+++ spring-api/src/test/kotlin/io/dodn/demo/MultiModulePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.dodn.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiModulePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
